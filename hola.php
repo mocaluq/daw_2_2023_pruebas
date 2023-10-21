25c329ff9c404a31eef466e7a73a6f9ca79a7285
[Dmocaluq[ <monica.s333237@cesurformacion.com>
2023-10-21 04:45:51 +0200

    [Dprueba

---
+++ hola.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Primer php</title>
+  </head>
+  <body>
+    <?php echo "Hola Paco campeon!!!! echo es una función que sirve para sacar algo por pantalla " ?>
+  </body>
+</html>
